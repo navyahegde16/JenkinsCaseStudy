465cc3308efdc890124f8d01f3f84b15b5dfd13b
Navya Hegde <61609810+navyahegde16@users.noreply.github.com>
2021-08-27 12:27:36 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@
 
 <h2>What Can JavaScript Do?</h2>
 
-<p id="demo">Hello World</p>
+<p id="demo">Hello World....</p>
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
